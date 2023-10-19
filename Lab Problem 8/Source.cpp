a630058790361a31244ccf693936a8c8692b1b60
l-dunbar <143094474+l-dunbar@users.noreply.github.com>
2023-10-19 04:03:41 -0400

    Completed P8-1

--- Lab Problem 8/Source.cpp
+++ Lab Problem 8/Source.cpp
@@ -9,18 +9,23 @@ int countVowels(string str);
 int main()
 {
 	string str;
-	cout << "enter string:" << endl;
-	getline(cin, str);
-	int x = countVowels(str);
-	cout << "number of vowels = " << x << endl;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, str);
+		if (str == "Q") break;
+		int x = countVowels(str);
+		cout << "number of vowels = " << x << endl;
+	}
 	return 0;
 }
 int countVowels(string str)
 {
 	int vowelCount = 0;
-	for (int i = 0; str[i] > i;i++)
-		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' 
+	for (int i = 0; str[i]; i++)
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A'
 			|| str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+		{
 			vowelCount++;
+}
 	return vowelCount;
 }
